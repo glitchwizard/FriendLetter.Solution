9d1c088dec03f46c89c911639351304d32c63287
Alex Garcia and Charley McGowan <alex@>
2018-11-26 11:10:33 -0800

    Using Models folder structure now

---
+++ FriendLetter/Models/LetterVariable.cs
@@ -0,0 +1,18 @@
+namespace FriendLetter.Models
+{
+  public class LetterVariable
+  {
+    private string _recipient;
+
+    public string GetRecipient()
+    {
+      return _recipient;
+    }
+
+    public void SetRecipient(string newRecipient)
+    {
+      _recipient = newRecipient;
+    }
+
+  }
+}
